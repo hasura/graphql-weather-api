51a65878f2822b79707327dbfba568648f027916
Makenna Smutz <makennasmutz@gmail.com>
2020-08-19 13:16:03 +0200

    allow introspection

--- index.js
+++ index.js
@@ -64,7 +64,12 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
